f40418574ec6415f69d9973ffb60b4dab155196b
hitohira <hitohirasakuno@gmail.com>
2021-02-14 10:39:12 +0900

    add Truck class

---
+++ src/main/java/com/tiscon/domain/Truck.java
@@ -0,0 +1,31 @@
+package com.tiscon.domain;
+
+import java.io.Serializable;
+
+public class Truck implements Serializable {
+
+    private int truckId;
+
+    private String truckType;
+
+    private int maxBox;
+
+    private int price;
+
+    public int getTruckId(){ return truckId; }
+
+    public void setTruckId(int truckId){ this.truckId = truckId; }
+
+    public String getTruckType(){ return truckType; }
+
+    public void setTruckType(String truckType) { this.truckType = truckType; }
+
+    public int getMaxBox() { return maxBox; }
+
+    public void setMaxBox(int maxBox) { this.maxBox = maxBox; }
+
+    public int getPrice() { return price; }
+
+    public void setPrice(int price) { this.price = price; }
+
+}
